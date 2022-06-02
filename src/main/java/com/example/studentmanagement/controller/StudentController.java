52a13cb84be1a9bb2c6534f495dfc60401a67362
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-06-02 16:18:37 +0300

    Annotations added and thymelead websites connected

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -3,7 +3,10 @@ package com.example.studentmanagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.studentmanagement.entity.Student;
 import com.example.studentmanagement.service.StudentService;
 
 @Controller
@@ -21,4 +24,20 @@ public class StudentController {
         model.addAttribute("students", studentService.getAlStudents());
         return "students";
     }
+
+    @GetMapping("/students/new")
+	public String createStudentForm(Model model) {
+		
+		// create student object to hold student form data
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "create_student";
+		
+	}
+
+    @PostMapping("/students")
+	public String saveStudent(@ModelAttribute("student") Student student) {
+		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
 }
