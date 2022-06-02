0fbc5760dc5c55fd0bf3c6c089e555739b8016db
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-06-02 16:46:46 +0300

    DeleteStudentbyId added

--- src/main/java/com/example/studentmanagement/service/StudentService.java
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -5,12 +5,14 @@ import java.util.List;
 import com.example.studentmanagement.entity.Student;
 
 public interface StudentService {
-    List<Student> getAlStudents();
+    List<Student> getAllStudents();
 
     Student saveStudent(Student student);
 
     Student getStudentById(Long id);
 	
 	Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
     
 }
